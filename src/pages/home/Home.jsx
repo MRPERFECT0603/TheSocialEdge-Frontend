00269c73311f4b130f6892132d0563f8da0e7194
MRPERFECT0603 <vivekshaurya62@gmail.com>
2023-11-14 19:01:55 +0530

    error rectified

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,6 @@
 import Stories from "../../components/stories/Stories"
 import Posts from "../../components/posts/Posts"
-import "./home.scss"
+import "./Home.scss"
 
 const Home = () => {
   return (

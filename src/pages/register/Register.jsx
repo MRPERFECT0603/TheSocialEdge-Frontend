eca5c0e3c950c5cb40e5482c92ce4d46a4a15c12
MRPERFECT0603 <vivekshaurya62@gmail.com>
2023-11-30 11:37:49 +0530

    ip changed register

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -20,7 +20,7 @@ const Register = () => {
     e.preventDefault();
 
     try {
-      await axios.post("http://13.49.223.232:8000/api/auth/register", inputs);
+      await axios.post("http://13.48.5.48:8000/api/auth/register", inputs);
     } catch (err) {
       setErr(err.response.data);
     }

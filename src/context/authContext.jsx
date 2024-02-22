0161fbdfac32e9ffecbb1d595aa2a37848c84010
Vivek Shaurya <97908880+MRPERFECT0603@users.noreply.github.com>
2024-02-22 22:54:58 +0530

    Update authContext.jsx

--- src/context/authContext.jsx
+++ src/context/authContext.jsx
@@ -9,7 +9,7 @@ export const AuthContextProvider = ({ children }) => {
   );
 
   const login = async (inputs) => {
-    const res = await axios.post("http://13.48.5.48:8000/api/auth/login" , inputs , {
+    const res = await axios.post("http://13.48.192.106:8000/api/auth/login" , inputs , {
       withCredentials: true ,
     });
 
